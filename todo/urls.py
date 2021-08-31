609e14aea42d06f82530aefb0efb0d4cfbcb90d5
takato <takato20010814@gmail.com>
2021-08-31 18:21:54 +0900

    url

--- todo/urls.py
+++ todo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
      path("list/0", views.ListViewBoolFalse.as_view()),
      path("list/1", views.ListViewBoolTrue.as_view()),
      path("retrieve/<str:pk>", views.RetrieveView.as_view()),
-     path("", views.CreateView.as_view()),
-     path("<str:pk>", views.UpdateView.as_view()),
-     path("<str:pk>", views.DeleteView.as_view()),
+     path("create", views.CreateView.as_view()),
+     path("update/<str:pk>", views.UpdateView.as_view()),
+     path("delete/<str:pk>", views.DeleteView.as_view()),
 ]
